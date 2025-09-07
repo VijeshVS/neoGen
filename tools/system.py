a990f3dcc4aebd64ff2fc4e4bb19c48cafe03cb1
“vijeshvs” <vijeshsshetty@gmail.com>
2025-09-07 10:55:40 +0530

    execute cmd at correct location

--- tools/system.py
+++ tools/system.py
@@ -1,5 +1,15 @@
 import subprocess
 
 def execute_command(cmd: str):
-    res = subprocess.check_output(cmd,shell=True,text=True)
-    return res
+    result = subprocess.run(
+        cmd,
+        cwd="/Users/vijeshshetty/Documents/projects/neoGen/generated_project",
+        shell=True,              # allow full shell commands like "npm init -y && npm install"
+        text=True,               # decode bytes -> str
+        capture_output=True      # capture stdout & stderr
+    )
+
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        return f"Error:\n{result.stderr.strip()}"
